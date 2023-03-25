c4a93f038ca3e6425aa6f75ff30c75af302d081c
wheanyichukwu <wheanyichukwupeters@gmail.com>
2023-03-25 08:19:29 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - print the number since 0 up to 9
- * Return: The number since 0 up to 9
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
